0f41007415ed8b6567e62fe5357fd6af85551ce6
Aryo Putra P <aryoputrap@gmail.com>
2020-12-04 04:14:30 +0800

    first commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+});
+
+const Fire = firebase;
+
+export default Fire;
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main = sebelum nh g ada
+// git remote add origin https://github.com/aryoputrap/doctorapp.git
+// git push -u origin main
