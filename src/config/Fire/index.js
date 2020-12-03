e86ee8e798b88f93e7d821d9ee57529c228ad496
Aryo Putra P <aryoputrap@gmail.com>
2020-12-04 04:16:18 +0800

    test commit

--- src/config/Fire/index.js
+++ src/config/Fire/index.js
@@ -14,9 +14,3 @@ const Fire = firebase;
 
 export default Fire;
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main = sebelum nh g ada
-// git remote add origin https://github.com/aryoputrap/doctorapp.git
-// git push -u origin main
